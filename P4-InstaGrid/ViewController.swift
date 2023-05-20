bbb7a1355958b313ca34a11c70e039be5434aaa2
RedouaneAmourBaracoda <redouane.amour@baracoda.com>
2023-05-20 12:50:32 +0200

    Initial Commit

---
+++ P4-InstaGrid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  P4-InstaGrid
+//
+//  Created by Redouane on 20/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ P4-InstaGridUITests/P4_InstaGridUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  P4_InstaGridUITestsLaunchTests.swift
+//  P4-InstaGridUITests
+//
+//  Created by Redouane on 20/05/2023.
+//
+
+import XCTest
+
+final class P4_InstaGridUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
